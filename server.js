c1f18f9cf4c1ebb3fed3a893757d306eab389d2a
storepreventerprakhar <ee16b061@smail.iitm.ac.in>
2017-02-05 10:16:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article-one', fuction(req, res){
-   res.send("article one is requested and will be served");
+app.get('/article-one', function(req, res) {
+   res.send('article one is requested and will be served');
 });
 
 app.get('/ui/style.css', function (req, res) {
